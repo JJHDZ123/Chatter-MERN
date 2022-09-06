12fe71738ce584b53732017aea804e1bdf243edf
Juan H <juantwothreeinseries@gmail.com>
2022-09-05 22:49:21 -0500

    finished log out and started messages component

--- client/src/components/chatcontainer/ChatContainer.js
+++ client/src/components/chatcontainer/ChatContainer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import ChatInput from './chatcomponents/ChatInput';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+
+import Logout from './chatcomponents/Logout';
+import Messages from './chatcomponents/Messages';
 
 function ChatContainer({ currentChat }) {
+	const handleSendMessage = async (msg) => {};
 	return (
 		<Container>
 			<div className="chat-header">
@@ -13,13 +19,39 @@ function ChatContainer({ currentChat }) {
 						<h3>{currentChat.username}</h3>
 					</div>
 				</div>
+				<div>
+					<Logout />
+				</div>
 			</div>
-			<div className="chat-messages"> CHAT MESSAGES </div>
-			<div className="chat-input"> CHAT INPUT </div>
+			<Messages />
+			<ChatInput handler={handleSendMessage} />
 		</Container>
 	);
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+	padding-top: 1rem;
+	.chat-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 2rem;
+	}
+	.user-details {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		.avatar {
+			img {
+				height: 3rem;
+			}
+		}
+		.username {
+			h3 {
+				color: white;
+			}
+		}
+	}
+`;
 
 export default ChatContainer;
